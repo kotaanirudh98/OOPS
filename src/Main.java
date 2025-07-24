41fa922eeb25e1a6b8ce32c56cba273bf0b80213
Anirudh kumar Kota <anirudhkumarkota@Anirudhs-MacBook-Pro.local>
2025-07-24 15:54:44 -0700

    commit for hub

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import User.User;
+
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        User s = new User("abhi",  "4807913088s");
+
+//        User s = new User();
+//        s.setUsername("abhi");
+//
+//        System.out.println(s.getUsername());
+//        System.out.println(s.getUserId());
+
+
+        User s2 = new User(s);
+
+        System.out.println("s " + s.getUsername());
+        System.out.println("s2" + s2.getUsername());
+
+        s2.setContactInfo("12345");
+        System.out.println("s2r" + s2.getContactInfo());
+        System.out.println("s1r" + s.getContactInfo());
+
+
+
+
+
+    }
+}
